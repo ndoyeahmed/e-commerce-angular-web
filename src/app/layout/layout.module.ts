580210806f60b32d2495f979f1c3a6ea8f9ce8a4
Mouhamed NDOYE <ndoyeahmed2602@gmail.com>
2019-12-30 21:47:30 +0000

    add menu component

--- src/app/layout/layout.module.ts
+++ src/app/layout/layout.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { MenuComponent } from './menu/menu.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [MenuComponent],
   imports: [
     CommonModule
   ]
